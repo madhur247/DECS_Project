bbae29402ee92c0333d4c430a5762f5b5e0d5768
madhur247 <25m0742@iitb.ac.in>
2025-10-31 16:49:34 +0000

    fixed few loadgen bugs

--- load_gen.cpp
+++ load_gen.cpp
@@ -31,7 +31,7 @@ void thread_handler(int seconds, int *thread_requests, float *thread_resptime){
     int count = 0;
     auto req_sent = chrono::steady_clock::now();
     auto resp_recvd = req_sent;
-    auto resp_time = chrono::duration_cast<chrono::microseconds>(resp_recvd-req_sent);
+    auto resp_time = chrono::duration_cast<chrono::milliseconds>(resp_recvd-req_sent);
     double total_resptime = 0;
     do {
         string command, key;
@@ -49,14 +49,14 @@ void thread_handler(int seconds, int *thread_requests, float *thread_resptime){
             req_sent = chrono::steady_clock::now();
             auto res = cli.Post("/create", body, "application/x-www-form-urlencoded");
             resp_recvd = chrono::steady_clock::now();
-            resp_time = chrono::duration_cast<chrono::microseconds>(resp_recvd-req_sent);
+            resp_time = chrono::duration_cast<chrono::milliseconds>(resp_recvd-req_sent);
             if (!res) cerr << "Request failed\n";
         } else if (cmd == 2) {
             string query = "/read?key=" + key;
             req_sent = chrono::steady_clock::now();
             auto res = cli.Get(query.c_str());
             resp_recvd = chrono::steady_clock::now();
-            resp_time = chrono::duration_cast<chrono::microseconds>(resp_recvd-req_sent);
+            resp_time = chrono::duration_cast<chrono::milliseconds>(resp_recvd-req_sent);
             if (!res) cerr << "Request failed\n";
         } else if(cmd == 3){
             string value = generateRandomString(val_len);
@@ -64,14 +64,14 @@ void thread_handler(int seconds, int *thread_requests, float *thread_resptime){
             req_sent = chrono::steady_clock::now();
             auto res = cli.Put("/update", body, "application/x-www-form-urlencoded");
             resp_recvd = chrono::steady_clock::now();
-            resp_time = chrono::duration_cast<chrono::microseconds>(resp_recvd-req_sent);
+            resp_time = chrono::duration_cast<chrono::milliseconds>(resp_recvd-req_sent);
             if (!res) cerr << "Request failed\n";
         } else if (cmd == 4) {
             string query = "/delete?key=" + key;
             req_sent = chrono::steady_clock::now();
             auto res = cli.Delete(query.c_str());
             resp_recvd = chrono::steady_clock::now();
-            resp_time = chrono::duration_cast<chrono::microseconds>(resp_recvd-req_sent);
+            resp_time = chrono::duration_cast<chrono::milliseconds>(resp_recvd-req_sent);
             if (!res) cerr << "Request failed\n";
         } else {
             cout << "Unknown command\n";
@@ -82,7 +82,7 @@ void thread_handler(int seconds, int *thread_requests, float *thread_resptime){
         duration = chrono::duration_cast<chrono::milliseconds>(now-start);
     } while(duration.count()<milli);
     *thread_requests = count;
-    *thread_resptime = total_resptime/1000;
+    *thread_resptime = total_resptime;
 }
 
 int main(int argc, char* argv[]) {
@@ -114,7 +114,7 @@ int main(int argc, char* argv[]) {
         total_requests+=requests[i];
         total_resp_time+=resptimes[i];
     }
-    cout<< "Avg throughput for "<<threads_num<<" threads = "<<(total_requests/total_resp_time/1000)<<"req/s"<<endl;
+    cout<< "Avg throughput for "<<threads_num<<" threads = "<<(total_requests/(total_resp_time/1000))<<"req/s"<<endl;
     cout<< "Avg response time for "<<threads_num<<" threads = "<<(total_resp_time/total_requests)<<"ms"<<endl;
     return 0;
 }
---
+++ test_server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+KV_SERVER="./server"          
+LOAD_GEN="./loadgen"      
+MYSQLD="/usr/sbin/mysqld"        
+
+echo "Starting MySQL on CPU 0..."
+taskset -c 8 $MYSQLD --defaults-file=/etc/mysql/my.cnf &> mysql.log &
+MYSQL_PID=$!
+sleep 5
+
+echo "Starting KV server on CPU 1..."
+taskset -c 0,1,2,3 $KV_SERVER &> kv.log &
+KV_PID=$!
+sleep 2
+
+echo "Starting load generator on CPU 2..."
+taskset -c 4,5,6,7 $LOAD_GEN $1 $2
+LOAD_EXIT=$?
+
+echo "Stopping KV server and MySQL..."
+kill $KV_PID
+kill $MYSQL_PID
+
+exit $LOAD_EXIT
