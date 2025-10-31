5f2d4cfc9847feee5d3a363bdec1e0b454a5af20
madhur247 <25m0742@iitb.ac.in>
2025-10-31 05:02:20 +0000

    Added random generation in load_gen

--- load_gen.cpp
+++ load_gen.cpp
@@ -4,32 +4,53 @@
 #include <random>
 using namespace std;
 
+string generateRandomString(size_t length) {
+    const string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    random_device rd;  // Non-deterministic random number generator
+    mt19937 gen(rd()); // Mersenne Twister engine
+    uniform_int_distribution<> dist(0, charset.size() - 1);
+
+    string result;
+    result.reserve(length);
+    for (size_t i = 0; i < length; ++i) {
+        result += charset[dist(gen)];
+    }
+    return result;
+}
+
 void thread_handler(){
     random_device rd;
     mt19937 gen(rd());
     httplib::Client cli("localhost", 8080);
 
     while (true) {
-        string command, key, value;
-        uniform_int_distribution<> dist(1, 4);
+        string command, key;
+        uniform_int_distribution<> dist(1, 5);
         int cmd = dist(gen);
-        uniform_int_distribution<> dist(1, 200);
+        uniform_int_distribution<> dist(1, 1000);
         int key_int  = dist(gen);
         string key = to_string(key_int);
+        uniform_int_distribution<> dist(1, 1000);
+        int val_len = dist(gen);
 
-        if (command == "create") {
-            cout << "Enter value: ";
-            cin >> value;
+        if (cmd == 1) {
+            string value = generateRandomString(val_len);
             string body = "key=" + key + "&value=" + value;
             auto res = cli.Post("/create", body, "application/x-www-form-urlencoded");
             if (res) cout << res->body << "\n";
             else cerr << "Request failed\n";
-        } else if (command == "read") {
+        } else if (cmd == 2) {
             string query = "/read?key=" + key;
             auto res = cli.Get(query.c_str());
             if (res) cout << res->body << "\n";
             else cerr << "Request failed\n";
-        } else if (command == "delete") {
+        } else if(cmd == 3){
+            string value = generateRandomString(val_len);
+            string body = "key=" + key + "&value=" + value;
+            auto res = cli.Put("/update", body, "application/x-www-form-urlencoded");
+            if (res) cout << res->body << "\n";
+            else cerr << "Request failed\n";
+        } else if (cmd == 4) {
             string query = "/delete?key=" + key;
             auto res = cli.Delete(query.c_str());
             if (res) cout << res->body << "\n";
