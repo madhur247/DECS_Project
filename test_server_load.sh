166283a3dd4e4c09328115a7173cd26a0c81e6a0
madhur247 <madhurthakkar247@gmail.com>
2025-11-22 19:38:53 +0530

    testing

--- test_server_load.sh
+++ test_server_load.sh
@@ -7,14 +7,14 @@ g++ src/reset_db.cpp -o reset_db -lmysqlclient
 g++ src/populate_db.cpp -o populate_db -lmysqlclient
 
 echo "Resetting Database..."
-# ./reset_db
+./reset_db
 
 if [ "$3" = "2" ]; then
 echo "Populating Database"
-# ./populate_db 1000000
+./populate_db 1000000
 elif [ "$3" = "3" ]; then
 echo "Populating Database"
-# ./populate_db 6000
+./populate_db 6000
 fi
 
 KV_SERVER="./server"          
@@ -23,17 +23,17 @@ MYSQLD="/usr/sbin/mysqld"
 
 echo "Starting Mysql..."
 MYSQL_PID=$(pidof mysqld)
-taskset -cp 4,5,6,7 $MYSQL_PID &> mysql.log &
+taskset -cp 0,1,2,3 $MYSQL_PID &> mysql.log &
 # MYSQL_PID=$!
 sleep 5
 
 echo "Starting KV server..."
-taskset -c 0,1 $KV_SERVER &> kv.log &
+taskset -c 8,9,10,11 $KV_SERVER &> kv.log &
 KV_PID=$!
 sleep 2
 
 echo "Starting load generator..."
-taskset -c 8,9,10,11 $LOAD_GEN $1 $2 $3
+taskset -c 4,5,6,7 $LOAD_GEN $1 $2 $3
 LOAD_EXIT=$?
 
 echo "Stopping KV server and MySQL..."
