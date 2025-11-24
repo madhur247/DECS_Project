ac08ee50eaced4f30bca964f79d75c1dfcb00090
madhur247 <madhurthakkar247@gmail.com>
2025-11-24 10:03:56 +0530

    final checks done

--- src/server.cpp
+++ src/server.cpp
@@ -203,7 +203,7 @@ void signal_handler(int signal){
 }
 
 size_t get_bucket_index(const string& user_id) {
-    return stoi(user_id) % NUM_CACHE_BUCKETS;
+    hash<string>{}(user_id) % NUM_CACHE_BUCKETS;
 }
 
 void create_handler(const httplib::Request& req, httplib::Response& res) {
